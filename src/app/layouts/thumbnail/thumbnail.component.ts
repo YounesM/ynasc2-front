f14dd7c42150725baa6e4712a414bca57ea2b765
YounesM <younes@mgharfaoui.com>
2017-03-03 10:21:50 +0000

    added thumbnail component

--- src/app/layouts/thumbnail/thumbnail.component.ts
+++ src/app/layouts/thumbnail/thumbnail.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['thumbnail.component.css']
 })
 export class ThumbnailComponent implements OnInit {
+  thumbnails: any[];
 
-  constructor() { }
+  constructor() {
+    //REST Call
+    this.thumbnails = [
+      { title: 'Lorem ipsum...', date: '1488532720', img: 'http://placehold.it/300x300', prev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa delectus dignissimos, dolor dolorem dolores eum ex facere inventore labore maiores natus omnis quos recusandae rerum ullam, ut voluptate voluptatum.' },
+      { title: 'Lorem ipsum...', date: '1488532720', img: 'http://placehold.it/300x300', prev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa delectus dignissimos, dolor dolorem dolores eum ex facere inventore labore maiores natus omnis quos recusandae rerum ullam, ut voluptate voluptatum.' },
+      { title: 'Lorem ipsum...', date: '1488532720', img: 'http://placehold.it/300x300', prev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa delectus dignissimos, dolor dolorem dolores eum ex facere inventore labore maiores natus omnis quos recusandae rerum ullam, ut voluptate voluptatum.' },
+      { title: 'Lorem ipsum...', date: '1488532720', img: 'http://placehold.it/300x300', prev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa delectus dignissimos, dolor dolorem dolores eum ex facere inventore labore maiores natus omnis quos recusandae rerum ullam, ut voluptate voluptatum.' },
+      { title: 'Lorem ipsum...', date: '1488532720', img: 'http://placehold.it/300x300', prev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa delectus dignissimos, dolor dolorem dolores eum ex facere inventore labore maiores natus omnis quos recusandae rerum ullam, ut voluptate voluptatum.' },
+      { title: 'Lorem ipsum...', date: '1488532720', img: 'http://placehold.it/300x300', prev: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam culpa delectus dignissimos, dolor dolorem dolores eum ex facere inventore labore maiores natus omnis quos recusandae rerum ullam, ut voluptate voluptatum.' }
+    ]
+  }
 
   ngOnInit() {
   }
